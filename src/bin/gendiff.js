8791f398f278e7d6fec284c16baab8aeb00d8a96
artkiller <koshevnikod@gmail.com>
2021-03-29 19:14:35 +0300

    first commit

---
+++ src/bin/gendiff.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import program from '../commander.js';
+
+program();
---
+++ src/commander.js
@@ -0,0 +1,8 @@
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+export default () => program.parse(process.argv);
