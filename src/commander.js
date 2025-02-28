e91b163fa45405549c77e069606a3c3c9c45e121
Anton <koshevnikod@gmail.com>
2025-02-28 13:45:15 +0300

    step two

--- src/commander.js
+++ src/commander.js
@@ -6,5 +6,9 @@ export default (() => {
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1');
 
+  program
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
+
   program.parse(process.argv);
 });
