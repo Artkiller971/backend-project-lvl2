262f69ceb80eed35a0603c24d17e3a84743a936b
Anton <koshevnikod@gmail.com>
2025-03-02 15:45:24 +0300

    step six

--- __tests__/generateDiff.test.js
+++ __tests__/generateDiff.test.js
@@ -11,14 +11,29 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 let json1Path;
 let json2Path;
+let yaml1Path;
+let yaml2Path;
+let expected;
 
 beforeEach(() => {
   json1Path = getFixturePath('json1.json');
   json2Path = getFixturePath('json2.json');
+  yaml1Path = getFixturePath('yaml1.yaml');
+  yaml2Path = getFixturePath('yaml2.yml');
+  expected = fs.readFileSync(getFixturePath('json1-2diff.txt'), 'utf-8').trim();
 });
 
-test('generateDiff', () => {
-  const expected = fs.readFileSync(getFixturePath('json1-2diff.txt'), 'utf-8').trim();
+test('generateDiff json', () => {
   const actual = generateDiff(parse(json1Path), parse(json2Path));
+  expect(actual).toEqual(expected);
+});
+
+test('generateDiff yaml', () => {
+  const actual = generateDiff(parse(yaml1Path), parse(yaml2Path));
+  expect(actual).toEqual(expected);
+});
+
+test('generateDiff yaml and json', () => {
+  const actual = generateDiff(parse(json1Path), parse(yaml2Path));
   expect(actual).toEqual(expected);
 });
--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,15 @@
 import * as fs from 'node:fs';
 import path from 'node:path';
+import yaml from 'js-yaml';
 
 const parse = (file, format = '.json') => {
   switch (format) {
     case '.json': {
       return JSON.parse(file);
+    }
+    case '.yaml':
+    case '.yml': {
+      return yaml.load(file);
     }
     default: {
       throw new Error('Unsupported format!');
